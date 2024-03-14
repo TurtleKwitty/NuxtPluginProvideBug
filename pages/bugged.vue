c39f33cf7e37858ce69c7a57d86dc039cf086882
TurtleKwitty <jerimo849@gmail.com>
2024-03-14 01:29:41 -0400

    Add the workaround to the bugged exmaple

--- pages/bugged.vue
+++ pages/bugged.vue
@@ -3,6 +3,10 @@
 		<p>$data is: {{ $data === undefined ? "undefined" : $data }}</p>
 		<p v-if="$data">True</p>
 		<p v-else>False</p>
+		<p>Workaround:</p>
+		<p>$data is: {{ $data.value === undefined ? "undefined" : $data.value }}</p>
+		<p v-if="$data.value">True</p>
+		<p v-else>False</p>
 	</div>
 </template>
 
